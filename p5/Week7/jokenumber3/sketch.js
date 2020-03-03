c4a5f44bc50918bee84f0bdd4d1d0ad776762549
Shawndub100 <54636146+Shawndub100@users.noreply.github.com>
2020-03-02 18:09:47 -0600

    765

---
+++ p5/Week7/jokenumber3/sketch.js
@@ -0,0 +1,31 @@
+var myState = 0
+var timer = 0
+
+function setup() {
+  // put setup code here
+  createCanvas(1000,800) ;
+}
+
+function draw() {
+  // put drawing code here
+  switch(myState){
+    case 0:
+    background('green')
+    textSize(60)
+    text('What is a parasite ', 200, 200);
+
+      timer++;
+      if (timer > 200){
+        myState = 1;
+        timer = 0;
+      }
+    break;
+
+    case 1:
+    background('blue')
+    text('SOMETHING YOU SEE', 300, 400);
+        text('IN PARIS', 300, 500);
+    break;
+
+  }
+}
